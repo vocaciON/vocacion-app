152fcdddeca3b7399c8b755eb7cea4d81ff9f7a3
MauricioF68 <mauricioterrones98@gmail.com>
2024-11-25 12:20:12 -0500

    feat: implementar del componente navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './shared/components/footer/footer.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FooterComponent],
+  imports: [RouterOutlet, FooterComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   standalone: true,
--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -2,10 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css'],
+  standalone: true
 })
-export class NavbarComponent {
-
-}
+export class NavbarComponent {}
