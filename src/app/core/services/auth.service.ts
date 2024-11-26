bd9dc9d5a47b01891dc4994846b21900bf3363ae
MauricioF68 <mauricioterrones98@gmail.com>
2024-11-26 18:46:01 -0500

    feat: implementando el auth.service

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,9 +1,39 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { StorageService } from './storage.service';
+import { AuthRequest } from '../../shared/models/auth-request.model';
+import { Observable, tap } from 'rxjs';
+import { AuthResponse } from '../../shared/models/auth.response.model';
+import { RegisterRequest } from '../../shared/models/register-request.moderl';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private baseURL = `${environment.baseURL}/auth`;
+  private http = inject(HttpClient);
+
+  private storageServie = inject(StorageService);
+
+  constructor() {}
+
+  login(authRequest: AuthRequest):Observable<AuthResponse>{
+    return this.http.post<AuthResponse>(`${this.baseURL}/login`, authRequest)
+    .pipe(
+      tap(response => this.storageServie.setAuthData(response))
+    );
+  }
+
+  register(registerRequest:RegisterRequest):Observable<RegisterResponse>{
+    return this.http.post<RegisterResponse>(`${this.baseURL}/register/perfil`, registerRequest)
+  }
+
+  logout(): void {
+    this.storageServie.clearAuthData();
+  }
+
+
+
 }
