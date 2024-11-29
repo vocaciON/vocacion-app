d4c656cf0ec6875ef80461a1ea4d755f648fdb92
MauricioF68 <mauricioterrones98@gmail.com>
2024-11-29 08:38:46 -0500

    Implementar componentes del propio layout del estudiante

---
+++ src/app/pages/estudiante/estudiante-layout/estudiante-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-estudiante-layout',
+  imports: [],
+  templateUrl: './estudiante-layout.component.html',
+  styleUrl: './estudiante-layout.component.css'
+})
+export class EstudianteLayoutComponent {
+
+}
