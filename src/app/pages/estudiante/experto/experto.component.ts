8c80fbd129e921f8a476a4dc427d3df175aab727
MauricioF68 <mauricioterrones98@gmail.com>
2024-11-29 15:10:33 -0500

    feat: Implementando ver los expertos

---
+++ src/app/pages/estudiante/experto/experto.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-experto',
+  imports: [],
+  templateUrl: './experto.component.html',
+  styleUrl: './experto.component.css'
+})
+export class ExpertoComponent {
+
+}
